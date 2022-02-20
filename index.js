35120795f78f05d4aad438d0f975f488e94d4533
Alfonso Lopez Cruz <alfonso2081lopez@gmail.com>
2022-02-20 16:20:05 -0500

    commit tree e4261d1a95f2b1f21620b4e39443d4590641a87c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+    
+const drivers = ['Antonia', 'Nuru',];
+const drivers2 = ['Amari','Mo']
+function returnFirstTwoDrivers(){
+    return drivers
+}
+function  returnLastTwoDrivers(){
+    return drivers2
+}
+function selectDifferentDrivers(drivers,drivers2){
+    return returnFirstTwoDrivers();
+    return returnLastTwoDrivers();
+}
+const fare = 5
+function createFareMultiplier(){
+    return function(fare){
+    fare * 5}
+}
+function fareDoubler(){
+    if (createFareMultiplier = 10){
+        return 10 * 2
+    }
+}
+function fareTripler(){
+    if (createFareMultiplier = 20){
+        return 4 * 9
+    }
+}
+
